4beb7d6f1bc5f56941e6c43e651c542647edfed5
clyvekokotiko <clyvekokotiko@gmail.com>
2022-06-05 00:45:15 +0200

    add chamber setup

---
+++ chamber/js/scripts.js
@@ -0,0 +1,15 @@
+function toggleMenu (){
+    document.getElementById("primary-nav").classList.toggle("open");
+    document.getElementById("hambuger-btn").classList.toggle("open");
+}
+
+document.getElementById("hambuger-btn").onclick = toggleMenu;
+
+let daylist = ["Sunday","Monday","Tuesday","Wednesday ","Thursday","Friday","Saturday"];
+let monthList = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+document.querySelector("#header-date-id").textContent = 
+`${daylist[new Date().getDay()]}, ${new Date().getDate()} ${monthList[new Date().getMonth() ]} ${new Date().getFullYear()}`;
+
+//footer
+document.querySelector("#last-modified").textContent = document.lastModified
